dc35e3363bb70481dec749dd6db6e6803f146fa3
Vangel Iliev <viliev@sidigitalsolutions.com>
2021-11-19 23:55:02 +0200

    Implementing Question Service

--- InterviewImplementation/QuestionService.cs
+++ InterviewImplementation/QuestionService.cs
@@ -1,6 +1,8 @@
 ﻿using AutoMapper;
 using DbEntities.DBContext;
+using DbEntities.Entities;
 using InterviewContracts;
+using Microsoft.EntityFrameworkCore;
 using Models;
 using System;
 using System.Collections.Generic;
@@ -18,34 +20,53 @@ namespace InterviewImplementation
             this._context = context;
         }
 
-        public Task<Guid> CreateAsync(QuestionServiceModel entity)
+        public async Task<Guid> CreateAsync(QuestionServiceModel entity)
         {
-            throw new NotImplementedException();
+            var questionDbModel = this._autoMapper.Map<Question>(entity);
+            this._context.Questions.Add(questionDbModel);
+            await this._context.SaveChangesAsync();
+            return questionDbModel.Id;
         }
 
-        public Task DeleteAsync(QuestionServiceModel entity)
+        public async Task DeleteAsync(QuestionServiceModel entity)
         {
-            throw new NotImplementedException();
+            var questionDbModel = this._autoMapper.Map<Question>(entity);
+            this._context.Questions.Remove(questionDbModel);
+            await this._context.SaveChangesAsync();
         }
 
-        public Task DeleteAsync(Guid id)
+        public async Task DeleteAsync(Guid id)
         {
-            throw new NotImplementedException();
+            var dbquestion = await this._context.Questions.FindAsync(id);
+            if (dbquestion != null)
+            {
+                this._context.Questions.Remove(dbquestion);
+                await this._context.SaveChangesAsync();
+            }
         }
 
-        public Task<List<QuestionServiceModel>> FindAllAsync()
+        public async Task<List<QuestionServiceModel>> FindAllAsync()
         {
-            throw new NotImplementedException();
+            var questions = await this._context.Questions.AsNoTracking().ToListAsync();
+            var questionsServiceModels = this._autoMapper.Map<List<QuestionServiceModel>>(questions);
+            return await Task.FromResult(questionsServiceModels);
         }
 
-        public Task<QuestionServiceModel> ReadAsync(Guid id)
+        public async Task<QuestionServiceModel> ReadAsync(Guid id)
         {
-            throw new NotImplementedException();
+            var question = await this._context.Questions.AsNoTracking().FirstOrDefaultAsync(x => x.Id == id);
+            var questionServiceModel = this._autoMapper.Map<QuestionServiceModel>(question);
+            return questionServiceModel;
         }
 
-        public Task UpdateAsync(QuestionServiceModel entity)
+        public async Task UpdateAsync(QuestionServiceModel entity)
         {
-            throw new NotImplementedException();
+            var question = await this._context.Questions.FindAsync(entity.Id);
+            if (question != null)
+            {
+                var dbEntity = this._autoMapper.Map<QuestionAnswer>(entity);
+                this._context.Update(dbEntity);
+            }
         }
     }
 }
